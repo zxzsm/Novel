8afb84ad2a9304f4b4bbe6b8c8fb814e2edf2fe5
Xiang <Xiang@DESKTOP-98U3TRH>
2018-11-27 17:15:35 +0800

    修改样式

--- Novel/Controllers/IndexController.cs
+++ Novel/Controllers/IndexController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Novel.Entity.Models;
+using Novel.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,11 +12,13 @@ namespace Novel.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            IndexViewModel viewModel = new IndexViewModel();
             using (BookContext bookContext = new BookContext())
             {
                 var t = bookContext.Book.ToList();
+                viewModel.Fantasy = t;
             }
-            return View();
+            return View(viewModel);
         }
     }
 }
---
+++ Novel/Models/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Novel.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novel.Models
+{
+    public class IndexViewModel
+    {
+        /// <summary>
+        /// 玄幻奇幻
+        /// </summary>
+        public List<Book> Fantasy { get; set; }
+    }
+}
