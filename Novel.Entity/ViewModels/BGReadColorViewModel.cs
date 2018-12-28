d509662c7c1e31754a54c46f09894c7c50d4a16e
Xiang <Xiang@DESKTOP-98U3TRH>
2018-12-28 17:50:29 +0800

    增加样式修改

---
+++ Novel.Entity/ViewModels/BGReadColorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Novel.Entity.ViewModels
+{
+    public class BGReadColorViewModel
+    {
+        public string color { get; set; }
+        public string colortitle { get; set; }
+    }
+}
---
+++ Novel.Entity/ViewModels/BookReadSettingViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Novel.Entity.ViewModels
+{
+    /// <summary>
+    /// 个性阅读设置保存
+    /// </summary>
+    public class BookReadSettingViewModel
+    {
+        public string bgcolor { get; set; }
+        public string fontfamily { get; set; }
+        public string fontsize { get; set; }
+
+    }
+}
